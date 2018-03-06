e1d7a0f7b80b3254a1d39ed32b5af991d6ebc80c
Heltonss <Helton@heltonsouza.com.br>
2018-03-05 22:25:34 -0300

    ordenando em crescente e decrescente

--- class/excel/js/script.js
+++ class/excel/js/script.js
@@ -22,26 +22,30 @@ var Excel = React.createClass({
     ),
     initialData: React.PropTypes.arrayOf(
       React.PropTypes.arrayOf(
-        React.PropTypes.any
+        React.PropTypes.string
       )
     )
   },
   getInitialState: function () {
     return {
       data: this.props.initialData,
-      sortBy: null,
+      sortby: null,
       descending: false
     }
   },
   _sort: function (e) {
-    var column = e.target.cellIndex;
-    //copia os dados
-    var data = this.state.data.slice();
-    data.sort(function (a, b) { 
-      return a[column] > b[column] ? 1 : -1; 
-     });
+    var column = e.target.cellIndex
+    console.log('col ', column)
+    // copia os dados
+    var data = this.state.data.slice()
+    var descending = this.state.sortby === column && !this.state.descending
+    data.sort(function (a, b) {
+      return descending ? (a[column] < b[column] ? 1 : -1) : (a[column] > b[column] ? 1 : -1)
+    })
     this.setState({
-      data: data
+      data: data,
+      sortby: column,
+      descending: descending
     })
   },
   render: function () {
@@ -50,10 +54,12 @@ var Excel = React.createClass({
       React.DOM.thead({onClick: this._sort},
         React.DOM.tr(null,
           this.props.headers.map(function (title, idx) {
+            if (this.state.sortby === idx) {
+              title += this.state.descending ? '\u2191' : '\u2193'
+            }
             return React.DOM.th({key: idx}, title)
-          })
-        )
-      ),
+          }, this)
+        )),
       React.DOM.tbody(null,
         this.state.data.map(function (row, idx) {
           return (
