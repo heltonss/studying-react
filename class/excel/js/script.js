9c168419db9a592a473f0d9bcc6bef3aafd6dc1c
Heltonss <Helton@heltonsouza.com.br>
2018-03-06 21:28:12 -0300

    planilha finalizada

--- class/excel/js/script.js
+++ class/excel/js/script.js
@@ -1,4 +1,104 @@
-// validando os dados e fornecendo sempre um array
+var Excel = React.createClass({
+  displayName: 'Excel',
+
+  propTypes: {
+    headers: React.PropTypes.arrayOf(
+      React.PropTypes.string
+    ),
+    initialData: React.PropTypes.arrayOf(
+      React.PropTypes.arrayOf(
+        React.PropTypes.string
+      )
+    )
+  },
+
+  getInitialState: function () {
+    return {
+      data: this.props.initialData,
+      sortby: null,
+      descending: false,
+      edit: null
+    }
+  },
+
+  _sort: function (e) {
+    var column = e.target.cellIndex
+    // copia os dados
+    var data = this.state.data.slice()
+    var descending = this.state.sortby === column && !this.state.descending
+    data.sort(function (a, b) {
+      return descending ? (a[column] < b[column] ? 1 : -1) : (a[column] > b[column] ? 1 : -1)
+    })
+    this.setState({
+      data: data,
+      sortby: column,
+      descending: descending
+    })
+  },
+
+  _showEditor: function (e) {
+    this.setState({edit: {
+        row: parseInt(e.target.dataset.row, 10),
+        cell: e.target.cellIndex
+    }})
+  },
+
+  _save: function (e) {
+    e.preventDefault()
+    var input = e.target.firstChild
+    var data = this.state.data.slice()
+    data[this.state.edit.row][this.state.edit.cell] = input.value
+
+    this.setState({
+      edit: null,
+      data: data
+    })
+  },
+
+  render: function () {
+    return (
+    React.DOM.table(null,
+      React.DOM.thead({onClick: this._sort},
+        React.DOM.tr(null,
+          this.props.headers.map(function (title, idx) {
+            if (this.state.sortby === idx) {
+              title += this.state.descending ? '\u2191' : '\u2193'
+            }
+            return React.DOM.th({key: idx}, title)
+          }, this)
+        )),
+
+      React.DOM.tbody({onDoubleClick: this._showEditor},
+        this.state.data.map(function (row, rowidx) {
+          return (
+          React.DOM.tr({key: rowidx},
+            row.map(function (cell, idx) {
+              var content = cell
+              var edit = this.state.edit
+
+              if (edit && edit.row === rowidx && edit.cell === idx) {
+                content = React.DOM.form({onSubmit: this._save},
+                  React.DOM.input({
+                    type: 'text',
+                    defaultValue: cell
+                  })
+                )
+              }
+
+              return React.DOM.td(
+                {
+                  key: idx,
+                  'data-row': rowidx
+                }, content)
+            }, this)
+          )
+          )
+        }, this)
+      )
+    )
+    )
+  }
+})
 
 var headers = [
   'Book', 'Author', 'Language', 'Published', 'Sales'
@@ -13,66 +113,6 @@ var data = [
   ['The Hobbit', 'J. R. R. Tolkien', 'English', '1937', '100 million'],
   ['She: A History of Adventure', 'H. Rider Haggard', 'English', '1887', '100 million']
 ]
-
-var Excel = React.createClass({
-  displayName: 'Excel',
-  propTypes: {
-    headers: React.PropTypes.arrayOf(
-      React.PropTypes.string
-    ),
-    initialData: React.PropTypes.arrayOf(
-      React.PropTypes.arrayOf(
-        React.PropTypes.string
-      )
-    )
-  },
-  getInitialState: function () {
-    return {
-      data: this.props.initialData,
-      sortby: null,
-      descending: false
-    }
-  },
-  _sort: function (e) {
-    var column = e.target.cellIndex
-    console.log('col ', column)
-    // copia os dados
-    var data = this.state.data.slice()
-    var descending = this.state.sortby === column && !this.state.descending
-    data.sort(function (a, b) {
-      return descending ? (a[column] < b[column] ? 1 : -1) : (a[column] > b[column] ? 1 : -1)
-    })
-    this.setState({
-      data: data,
-      sortby: column,
-      descending: descending
-    })
-  },
-  render: function () {
-    return (
-    React.DOM.table(null,
-      React.DOM.thead({onClick: this._sort},
-        React.DOM.tr(null,
-          this.props.headers.map(function (title, idx) {
-            if (this.state.sortby === idx) {
-              title += this.state.descending ? '\u2191' : '\u2193'
-            }
-            return React.DOM.th({key: idx}, title)
-          }, this)
-        )),
-      React.DOM.tbody(null,
-        this.state.data.map(function (row, idx) {
-          return (
-          React.DOM.tr({key: idx},
-            row.map(function (cell, idx) {
-              return React.DOM.td({key: idx}, cell)
-            })
-          ))
-        })
-      )
-    ))
-  }
-})
 
 ReactDOM.render(
   React.createElement(Excel, {
